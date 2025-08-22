a2cd7a6f53734d9450c3765fc197e093f367bc47
Kzinn1810 <kauafelipe18102007@gmail.com>
2025-08-22 14:03:15 -0300

    arte e gourmet

---
+++ produtos.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1,
+        name: 'Bolo de Pote de chocolate',
+        desc: 'Bolo de pote de chocolate com recheio cremoso.',
+        price: 10.0,
+        img: 'imagens/bolo-de-chocolate.PNG'
+    },
+    {
+        id: 2,
+        name: 'Brownie De chocolate com ninho',
+        desc: 'Brownie de chocolate com leite ninho e cobertura de chocolate.',
+        price: 10.0,
+        img: 'imagens/brownie-de-ninho-com-chocolate.PNG'
+    },
+    {
+        id: 3,
+        name: 'Tortinha de Maracujá',
+        desc: 'Tortinha de maracujá com recheio cremoso e cobertura de chocolate.',
+        price: 8.5,
+        img: 'imagens/tortinha-de-maracuja.PNG'
+    }
+];
